b7113ee310d819cfd1331bfdb501c80b5a62a59e
B3l99 <spekky189@gmail.com>
2025-10-24 19:00:29 +0200

    Add files via upload

---
+++ ProgramacionPython1/# Prueba básica de Python.py
@@ -0,0 +1,8 @@
+#en python no hace falta declarar las constantes como podemos ver aqui
+s = 0
+print(s)
+s = s + 2
+if s > 0:
+    print("But I don't want ANY pollita!")
+aa = "zasca en la boquita " * s
+print(aa)
